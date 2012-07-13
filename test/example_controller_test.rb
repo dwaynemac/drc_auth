0ad2f09f8daa257a056245d6ef4b82542702f68f
Dwayne Macgowan <dwaynemac@gmail.com>
2012-07-13 20:10:15 -0300

    test redirection

--- test/example_controller_test.rb
+++ test/example_controller_test.rb
@@ -4,5 +4,6 @@ class ExampleControllerTest < ActionController::TestCase
   test "should redirect to DRC" do
     get :index
     assert_response :redirect
+    assert_redirected_to "https://metododerose.org:8443"
   end
 end
